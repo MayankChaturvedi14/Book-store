2646759a1930bbd8518d81cf79018edf6c5d1aaa
MayankChaturvedi14 <mayankchaturvedi565@gmail.com>
2022-04-08 18:42:11 +0530

    Second Commit

--- Hello/settings.py
+++ Hello/settings.py
@@ -78,9 +78,10 @@ WSGI_APPLICATION = 'Hello.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation
